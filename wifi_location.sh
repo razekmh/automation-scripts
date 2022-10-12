23c845b1fd100a9952146744f631a6b9a3ef21df
Mahmoud Abdelrazek <razekmh@gmail.com>
2022-10-12 12:14:20 +0100

    fix name error

--- wifi_location.sh
+++ wifi_location.sh
@@ -1,6 +1,6 @@
-home_network_name='SHELLCDE686'
+#home_network_name is read from global variables
 
-if [[ $(/Sy*/L*/Priv*/Apple8*/V*/C*/R*/airport -I | grep -w SSID | awk '{print $2$3}') = $home_network_nam ]] && [[ $(/usr/sbin/networksetup -getcurrentlocation) != 'Home' ]]; then
+if [[ $(/Sy*/L*/Priv*/Apple8*/V*/C*/R*/airport -I | grep -w SSID | awk '{print $2$3}') = $home_network_name ]] && [[ $(/usr/sbin/networksetup -getcurrentlocation) != 'Home' ]]; then
         /usr/sbin/networksetup -switchtolocation "Home"
 fi
 
